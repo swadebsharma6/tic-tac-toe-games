35a14f60cd53fe0a0a9c76c2d843ef08d934e289
SwadebDeb sharma <swadebriva2012@gmail.com>
2025-05-16 17:48:11 +0600

    create board and square component and passing value by props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-
-
-const App = () => {
+const Square = ({value}) => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <button className="w-16 h-16 m-2 bg-cyan-500 rounded-md border border-gray-200">
+     {value}
+    </button>
   );
 };
 
-export default App;
+const Board = () => {
+  return (
+    <>
+      <div>
+        <Square value="1"/>
+        <Square value="2" />
+        <Square value="3"/>
+        
+      </div>
+      <div>
+        <Square value="4"/>
+        <Square value="5"/>
+        <Square value="6"/>
+      </div>
+      <div>
+       <Square value="7"/>
+       <Square value="8"/>
+       <Square value="9"/>
+      </div>
+    </>
+  );
+};
+
+export default Board;
